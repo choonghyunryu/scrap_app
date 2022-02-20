c61433bc672ed32a03da9689968684911ff7571d
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2022-02-20 16:05:05 +0900

    add action button

--- app.R
+++ app.R
@@ -14,10 +14,10 @@ ui <- fluidPage(
         sidebarPanel(
             textInput("client_id", 
                       label = h4("Client ID:"), 
-                      value = ""),
+                      value = "t9iL0Yqma3NV8lTJow87"),
             textInput("client_secret", 
                       label = h4("Client Secret:"), 
-                      value = ""),            
+                      value = "ZcBUuT5ElO"),            
             textInput("keyword", 
                       label = h4("검색 키워드:"), 
                       value = ""),
@@ -26,8 +26,8 @@ ui <- fluidPage(
                          selected = "date"),           
             sliderInput("max_record", label = h4("검색 건수:"), min = 0, 
                         max = 500, value = 100, step = 100),
-            # actionButton("search_keyword", label = "뉴스 검색", 
-            #              icon = icon("newspaper")),
+            actionButton("search_keyword", label = "뉴스 검색", 
+                         icon = icon("newspaper")),
             width = 3
         ),
 
@@ -41,38 +41,69 @@ ui <- fluidPage(
 
 # Server Side
 server <- function(input, output) {
+    newsList <- reactiveValues(
+        list = data.frame(
+            title = character(0),
+            description = character(0),
+            publish_date = character(0),
+            link = character(0),
+            stringsAsFactors = FALSE
+        ) %>% 
+            reactable(
+                defaultColDef = colDef(
+                    align = "left"
+                ),
+                columns = list(
+                    title = colDef(
+                        name = "타이틀",
+                        width = 250,
+                    ),
+                    description = colDef(name = "뉴스내용 요약"),
+                    publish_date = colDef(
+                        name = "뉴스 계시시간",
+                        width = 150,
+                    ),
+                    link = colDef(
+                        name = "뉴스 링크",
+                        width = 250,
+                        html = TRUE,
+                        cell = function(url) {
+                            htmltools::tags$a(href = as.character(url), target = "_blank", as.character(url))
+                        }
+                    )    
+                ),
+                showPageSizeOptions = TRUE,
+                pageSizeOptions = c(5, 10, 15), 
+                defaultPageSize = 5,
+                bordered = TRUE,
+                highlight = TRUE
+            )    
+    )
+    
     output$news_list <- renderReactable({
+        newsList$list
+    })
+    
+    observeEvent(input$search_keyword, {
         # 3개의 텍스트는 반드시 입력해야 함
         req(input$keyword)
         req(input$client_id)
         req(input$client_secret)
-        
-        # # 뉴스 검색 리스트의 초기화 출력
-        # init_data <- data.frame(
-        #     title = character(0),
-        #     description = character(0),
-        #     publish_date = character(0),
-        #     link = character(0),
-        #     stringsAsFactors = FALSE
-        # )
-        
-        # 네이버 오픈 API 호출
-        search_list <- koscrap::search_naver(
-            query = input$keyword, 
-            sort  = input$sort,
-            chunk = min(input$max_record, 100),
-            max_record = input$max_record,
-            do_done = TRUE,
-            client_id = input$client_id, 
-            client_secret = input$client_secret) %>%
-            mutate(title = title_text) %>% 
-            mutate(description = description_text) %>% 
-            mutate(publish_date = stringr::str_remove_all(publish_date, 
-                                                          "[[:alpha:]]")) %>% 
-            select(title, description, publish_date, link)
-        
+
+        result <- koscrap::search_naver(
+                query = input$keyword,
+                sort  = input$sort,
+                chunk = min(input$max_record, 100),
+                max_record = input$max_record,
+                do_done = TRUE,
+                client_id = input$client_id,
+                client_secret = input$client_secret) %>%
+                mutate(title = title_text) %>%
+                mutate(description = description_text) %>%
+                mutate(publish_date = stringr::str_remove_all(publish_date,
+                                                              "[[:alpha:]]")) %>%
+                select(title, description, publish_date, link) %>% 
         reactable(
-            search_list,
             defaultColDef = colDef(
                 align = "left"
             ),
@@ -101,32 +132,9 @@ server <- function(input, output) {
             bordered = TRUE,
             highlight = TRUE
         )
+        
+        newsList$list <- result
     })
-    
-    # observeEvent(input$search_keyword, {
-    #     # 3개의 텍스트는 반드시 입력해야 함
-    #     req(input$keyword)
-    #     req(input$client_id)
-    #     req(input$client_secret)
-    # 
-    #     # 네이버 오픈 API 호출
-    #     search_list <- koscrap::search_naver(
-    #         query = input$keyword,
-    #         sort  = input$sort,
-    #         chunk = min(input$max_record, 100),
-    #         max_record = input$max_record,
-    #         do_done = TRUE,
-    #         client_id = input$client_id,
-    #         client_secret = input$client_secret) %>%
-    #         mutate(title = title_text) %>%
-    #         mutate(description = description_text) %>%
-    #         mutate(publish_date = stringr::str_remove_all(publish_date,
-    #                                                       "[[:alpha:]]")) %>%
-    #         select(title, description, publish_date, link)
-    # 
-    #     updateReactable("news_list",
-    #                     data = search_list)
-    # })
 }
 
 # Run the application 
